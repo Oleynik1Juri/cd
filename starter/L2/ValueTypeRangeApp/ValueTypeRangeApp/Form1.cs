1470d49b6b7c2dca2453db80e070ecdde0af5ce4
Juri <oleynikjuri@outlook.com>
2019-08-19 22:22:49 +0300

    Empty ValueTypeRangeApp

--- starter/L2/ValueTypeRangeApp/ValueTypeRangeApp/Form1.cs
+++ starter/L2/ValueTypeRangeApp/ValueTypeRangeApp/Form1.cs
@@ -16,5 +16,15 @@ namespace ValueTypeRangeApp
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
