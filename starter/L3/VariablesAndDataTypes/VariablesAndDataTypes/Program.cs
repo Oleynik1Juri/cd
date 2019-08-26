ad8c0667e1e27d5432e249c69cbd090a8da8fb70
Juri <oleynikjuri@outlook.com>
2019-08-26 21:03:00 +0300

    Arithmetic Calculation

--- starter/L3/VariablesAndDataTypes/VariablesAndDataTypes/Program.cs
+++ starter/L3/VariablesAndDataTypes/VariablesAndDataTypes/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace VariablesAndDataTypes
 {
@@ -10,6 +6,23 @@ namespace VariablesAndDataTypes
     {
         static void Main(string[] args)
         {
+            byte c = 4, d = 5;
+            int difference = d - c;
+            Console.WriteLine(difference);
+           
+            byte q = 7, w = 2;
+            int summand = q + w;
+            Console.WriteLine(summand);          
+            
+            byte r = 3, t = 14;
+            int multiplication = r * t;
+            Console.WriteLine(multiplication);
+            
+            byte s = 5, n = 10;
+            int division = s / n, remainder = s % n;
+            Console.WriteLine(division);
+                                
+            Console.ReadKey();
         }
     }
 }
