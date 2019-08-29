1b16688c2a93e3eee60ca6847cb5c0dc0496a4fc
Juri <oleynikjuri@outlook.com>
2019-08-29 20:36:40 +0300

    Translator Program

--- starter/L4/EmptyAppConditionalConstructions/EmptyAppConditionalConstructions/Program.cs
+++ starter/L4/EmptyAppConditionalConstructions/EmptyAppConditionalConstructions/Program.cs
@@ -1,15 +1,79 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EmptyAppConditionalConstructions
+﻿namespace EmptyAppConditionalConstructions
 {
     class Program
     {
         static void Main(string[] args)
+
         {
+            string word;
+            System.Console.WriteLine("Добро пожаловать в русско-английский переводчик");
+            do
+            {
+                System.Console.ForegroundColor = System.ConsoleColor.White;
+                System.Console.WriteLine("Введите русское слово: ");
+                word = System.Console.ReadLine();
+                switch (word)
+                {
+                    case "небо":
+                        {
+                            System.Console.WriteLine("Sky");
+                            break;
+                        }
+                    case "снег":
+                        {
+                            System.Console.WriteLine("Snow");
+                            break;
+                        }
+                    case "туман":
+                        {
+                            System.Console.WriteLine("fog");
+                            break;
+                        }
+                    case "осадки":
+                        {
+                            System.Console.WriteLine("rainfall");
+                            break;
+                        }
+                    case "дождь":
+                        {
+                            System.Console.WriteLine("rain");
+                            break;
+                        }
+                    case "ливень":
+                        {
+                            System.Console.WriteLine("shower");
+                            break;
+                        }
+                    case "холодный":
+                        {
+                            System.Console.WriteLine("chilly");
+                            break;
+                        }
+                    case "моросит":
+                        {
+                            System.Console.WriteLine("drizzle");
+                            break;
+                        }
+                    case "ураган":
+                        {
+                            System.Console.WriteLine("hurricane");
+                            break;
+                        }
+                    case "молния":
+                        {
+                            System.Console.WriteLine("lightning");
+                            break;
+                        }
+                    default:
+                        {
+                            System.Console.ForegroundColor = System.ConsoleColor.Green;
+                            System.Console.WriteLine("Вы ввели слово которого переводчик не знает");
+                            break;
+                        }
+                }
+
+            } while (word != "exit");
+
         }
     }
 }
