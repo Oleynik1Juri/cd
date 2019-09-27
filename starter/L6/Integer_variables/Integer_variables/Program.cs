df9d5432899f96ce8faf1ebb2554c5875d5ca0c3
OleynikJuri <Oleynikjuri@outlook.com>
2019-09-27 04:38:53 -0700

    rectangle of stars

--- starter/L6/Integer_variables/Integer_variables/Program.cs
+++ starter/L6/Integer_variables/Integer_variables/Program.cs
@@ -1,10 +1,24 @@
 ﻿using System;
+using System.Runtime.Serialization.Formatters;
+
 namespace Integer_variables
 {
     class Program
     {
         static void Main(string[] args)
         {
+            {
+                for (int a = 0; a < 7; a++)
+                {
+                   for (int b = 0; b < a; b++)
+                   {
+                        Console.Write("*");
+                   }
+                   Console.WriteLine();
+                }
+                Console.ReadKey();
+            }
         }
     }
 }
+
