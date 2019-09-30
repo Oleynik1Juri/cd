afcee8385aa7853ab377e1d16699b7fd8af42d28
OleynikJuri <Oleynikjuri@outlook.com>
2019-09-30 10:29:41 +0300

    Dividing each argument by 5

--- starter/L8/DivisionOfArgumentByFive/DivisionOfArgumentByFive/Program.cs
+++ starter/L8/DivisionOfArgumentByFive/DivisionOfArgumentByFive/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DivisionOfArgumentByFive
 {
@@ -10,6 +6,41 @@ namespace DivisionOfArgumentByFive
     {
         static void Main(string[] args)
         {
+            int number1, numberb2, number3;
+            Console.Write("Enter the first number:  ");
+            int.TryParse(Console.ReadLine(), out number1);
+            Console.Write("Enter the second number: ");
+            int.TryParse(Console.ReadLine(), out numberb2);
+            Console.Write("Enter the third number: ");
+            int.TryParse(Console.ReadLine(), out number3);
+            Calculate(number1, numberb2, number3);
+        }
+
+        static void Calculate(int number1, int numberb2, int number3)
+        {
+            if (number1 % 5 == 0) // Если number1 остаток % равно 0; 
+
+                Console.WriteLine($"First number={number1/5}"); // то number1 делится на 5 
+            else
+            {
+                Console.WriteLine("Error the first number is not divisible by 5"); // если остаток не равно 0, то Error
+            }
+            if (numberb2 % 5 == 0)
+
+                Console.WriteLine($"Second number={numberb2 / 5}");
+            else
+            {
+                Console.WriteLine("Error the second number is not divisible by 5");
+            }
+            if (number3 % 5 == 0)
+
+                Console.WriteLine($"Third number={number3 / 5}");
+            else
+            {
+                Console.WriteLine("Error the third number is not divisible by 5");
+            }
+            Console.ReadKey();
         }
     }
 }
+        
