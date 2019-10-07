5e98b75add8ce3ccc2540419aee22b7158622843
OleynikJuri <Oleynikjuri@outlook.com>
2019-10-07 22:44:03 +0300

    clean code

--- starter/L9/ArrayElementsInReverseOrder/ArrayElementsInReverseOrder/Program.cs
+++ starter/L9/ArrayElementsInReverseOrder/ArrayElementsInReverseOrder/Program.cs
@@ -1,40 +1,56 @@
 ﻿using System;
 
-namespace Arrays
+namespace ArrayElementsInReverseOrder
 {
     class Program
     {
         static void Main()
         {
-            int[] array = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
-            array[0] = 1;
-            array[1] = 2;
-            array[2] = 3;
-            array[3] = 4;
-            array[4] = 5;
-            array[5] = 6;
-            array[6] = 7;
-            array[7] = 8;
-            array[8] = 9;
-            array[9] = 10;
 
-            Console.WriteLine(array[9]);
-            Console.WriteLine(array[8]);
-            Console.WriteLine(array[7]);
-            Console.WriteLine(array[6]);
-            Console.WriteLine(array[5]);
-            Console.WriteLine(array[4]);
-            Console.WriteLine(array[3]);
-            Console.WriteLine(array[2]);
-            Console.WriteLine(array[1]);
-            Console.WriteLine(array[0]);
-
-            for (int i = 0; i < array.Length; i++)
-            {
-                array[i] = i + 1;
-            }
-                      
+            var array = SsArray(10);
+            Display(array);
+            Console.WriteLine("-----------------");
+            var invert = InArray(array);
+            Display(invert);
             Console.ReadKey();
+
+        }
+
+        static int[] SsArray(int n)
+        {
+            var array = new int[n];
+
+            for (var i = 0; i < n; i++)
+            {
+                array[i] = i;
+
+            }
+
+            return array;
+        }
+
+        static void Display(int[] array)
+        {
+            for (int d = 0; d < array.Length; d++)
+            {
+                Console.WriteLine(array[d]);
+            }
+
+        }
+
+        static int[] InArray(int[] array)
+        {
+            var n = array.Length;
+            var InArray = new int[n];
+
+            for (var i = 0; i < array.Length; i++)
+            {
+                InArray[i] = array[n - 1];
+                n--;
+                
+            }
+            
+            return InArray;
         }
     }
 }
