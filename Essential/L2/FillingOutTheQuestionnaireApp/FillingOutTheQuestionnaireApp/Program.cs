743c0c56a9965b3f6662fc8e98043127e12a680b
OleynikJuri <Oleynikjuri@outlook.com>
2019-12-28 20:06:57 +0200

    User information App

---
+++ Essential/L2/FillingOutTheQuestionnaireApp/FillingOutTheQuestionnaireApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FillingOutTheQuestionnaireApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
