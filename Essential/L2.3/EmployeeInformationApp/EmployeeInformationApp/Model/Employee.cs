c9ae5dee4f3838014f4b3d4486785c95a95d9123
OleynikJuri <Oleynikjuri@outlook.com>
2020-01-18 12:14:25 +0200

    Add DateTime

--- Essential/L2.3/EmployeeInformationApp/EmployeeInformationApp/Model/Employee.cs
+++ Essential/L2.3/EmployeeInformationApp/EmployeeInformationApp/Model/Employee.cs
@@ -1,18 +1,18 @@
 ﻿using System;
-
 namespace EmployeeInformationApp.Model
 {
     public class Employee
     {
         private readonly string _name;
         private readonly string _surname;
-
-        public Employee(string name, string surname)
+        private readonly DateTime _startWorkingDate;
+        
+        public Employee(string name, string surname, DateTime startWorkingDate)
         {
             _name = name;
             _surname = surname;
+            _startWorkingDate = startWorkingDate;
         }
-       
         public double TotalSalary()
         {
             var totalSalary = (1000 + 365) * 0.195;
