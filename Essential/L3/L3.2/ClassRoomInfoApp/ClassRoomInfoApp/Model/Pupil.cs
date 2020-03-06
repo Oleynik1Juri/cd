cbe065e4a2dca76f868f92a12e5929636aa3fb6f
OleynikJuri <Oleynikjuri@outlook.com>
2020-03-06 18:57:18 +0200

    method creation

---
+++ Essential/L3/L3.2/ClassRoomInfoApp/ClassRoomInfoApp/Model/Pupil.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace ClassRoomInfoApp.Model
+{
+    class Pupil
+    {
+        public void Study()
+        {
+
+        }
+
+        public void Read()
+        {
+
+        }
+
+        public void Write()
+        {
+
+        }
+
+        public void Relax()
+        {
+
+        }
+
+    }
+}
---
+++ Essential/L3/L3.2/ClassRoomInfoApp/ClassRoomInfoApp/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ClassRoomInfoApp
+{
+    class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}
