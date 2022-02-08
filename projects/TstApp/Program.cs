80fbc6f1b88f6080673a7a69b59f410834329693
OleynikJuri <Oleynikjuri@outlook.com>
2022-02-08 18:54:40 +0200

    TstApp added new empty project.

---
+++ projects/TstApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World Project");
+        }
+    }
+}
