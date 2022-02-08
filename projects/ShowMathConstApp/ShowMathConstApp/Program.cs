4d62694f56eec01652a866866b8a1f727dfdc143
OleynikJuri <Oleynikjuri@outlook.com>
2022-02-08 22:19:02 +0200

    Output value without loss App

--- projects/ShowMathConstApp/ShowMathConstApp/Program.cs
+++ projects/ShowMathConstApp/ShowMathConstApp/Program.cs
@@ -8,8 +8,13 @@ namespace ShowMathConstApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            const double pi = 3.141592653;
+            const double e = 2.7182818284590452;
+            Console.WriteLine(value: pi);
+            Console.WriteLine(value: e);
+            Console.ReadKey();
 
         }
     }
