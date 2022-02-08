690664e12404890edb2327ca3391aa6307171301
OleynikJuri <Oleynikjuri@outlook.com>
2022-02-09 00:00:25 +0200

    Calculation of mathematical exercises and display App

--- projects/Calculation of mathematical operations App/Calculation of mathematical operations App/Program.cs
+++ projects/Calculation of mathematical operations App/Calculation of mathematical operations App/Program.cs
@@ -9,7 +9,18 @@ namespace Calculation_of_mathematical_operations_App
     class Program
     {
         static void Main(string[] args)
-        {
+        { 
+            int x = 10, y = 12, z = 3;
+            Console.WriteLine("Exercise 1 = {0}", x += y - x++ * z);
+            x = 10; y = 12; z = 3;
+            Console.WriteLine("Exercise 2 = {0}", z = --x - y * 5);
+            x = 10; y = 12; z = 3;
+            Console.WriteLine("Exercise 3 = {0}", y /= x + 5 % z);
+            x = 10; y = 12; z = 3;
+            Console.WriteLine("Exercise 4 = {0}", z = x++ + y * 5);
+            x = 10; y = 12; z = 3;
+            Console.WriteLine("Exercise 5 = {0}", x = y - x++ * z);
+            Console.ReadKey();
         }
     }
 }
