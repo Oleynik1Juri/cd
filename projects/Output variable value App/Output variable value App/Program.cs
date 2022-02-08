71168251ba7f01d257b6e8c1c34d64233b488d9c
OleynikJuri <Oleynikjuri@outlook.com>
2022-02-08 22:27:33 +0200

    Output variable value App

---
+++ projects/Output variable value App/Output variable value App/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Output_variable_value_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
