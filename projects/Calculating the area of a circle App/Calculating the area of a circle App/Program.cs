3fb5102c89eb2ab4030c3ff689bc184930553e9d
OleynikJuri <Oleynikjuri@outlook.com>
2022-02-09 14:10:06 +0200

    Calculating the area of a circle and displaying it on the screen App

--- projects/Calculating the area of a circle App/Calculating the area of a circle App/Program.cs
+++ projects/Calculating the area of a circle App/Calculating the area of a circle App/Program.cs
@@ -10,6 +10,14 @@ namespace Calculating_the_area_of_a_circle_App
     {
         static void Main(string[] args)
         {
+            const double pi = 3.14;
+            Console.WriteLine("pi = 3.14");
+            Console.Write("r = ");
+            int.TryParse(Console.ReadLine(), out var r);
+            var s = pi * Math.Pow(r, 2);
+            Console.WriteLine("Area of a circle = {0}", s);
+            Console.ReadKey();
+
         }
     }
 }
