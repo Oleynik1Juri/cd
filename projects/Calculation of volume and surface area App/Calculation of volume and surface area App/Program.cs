6d2a029d4a1f2b421c4ea552d007d9f4dfc2f6ef
OleynikJuri <Oleynikjuri@outlook.com>
2022-02-09 14:19:05 +0200

    Calculation of volume and surface area App

---
+++ projects/Calculation of volume and surface area App/Calculation of volume and surface area App/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculation_of_volume_and_surface_area_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
