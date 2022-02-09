b62e0076cc090fc8d43731e6027b0c016f08c61c
OleynikJuri <Oleynikjuri@outlook.com>
2022-02-09 14:41:56 +0200

    Volume and surface area calculation and display App

--- projects/Calculation of volume and surface area App/Calculation of volume and surface area App/Program.cs
+++ projects/Calculation of volume and surface area App/Calculation of volume and surface area App/Program.cs
@@ -10,6 +10,22 @@ namespace Calculation_of_volume_and_surface_area_App
     {
         static void Main(string[] args)
         {
+            double pi = 3.14;
+            Console.WriteLine("Объем V цилиндра");
+            Console.Write("r = ");
+            int.TryParse(Console.ReadLine(), out var r);
+            Console.Write("h = ");
+            int.TryParse(Console.ReadLine(), out var h);
+            var v = pi * Math.Pow(r, 2) * h;
+            Console.WriteLine("Результат = {0}", v);
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("Площадь S поверхности цилиндра");
+            var s = 2 * pi * r * (r + h);
+            Console.WriteLine("Результат = {0}", s);
+            
+            Console.ReadKey();
+
+
         }
     }
 }
