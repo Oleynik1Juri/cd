7cf62ccd3184e393bf8f022013243ddbe052b11d
OleynikJuri <Oleynikjuri@outlook.com>
2022-02-10 17:24:38 +0200

    ConsoleCalculatorApp

--- projects/ConsoleCalculator/ConsoleCalculator/Program.cs
+++ projects/ConsoleCalculator/ConsoleCalculator/Program.cs
@@ -8,8 +8,51 @@ namespace ConsoleCalculator
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            
+            double operand2;
+
+            Console.WriteLine("Введите первое число: ");
+            Console.Write("");
+            int operand1;
+            int.TryParse(Console.ReadLine(), out operand1);
+
+            Console.WriteLine("Ввести арифметический знак: ");
+            var sign = Console.ReadLine();
+
+            Console.Write("Введите второе число: ");
+            Console.Write("");
+            double.TryParse(Console.ReadLine(), out operand2);
+
+            if (operand2 == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Делить на ноль нельзя");
+            }
+            else
+            {
+                switch (sign)
+                {
+                    case "+":
+                        Console.WriteLine($"{operand1 + operand2}");
+                        break;
+                    case "-":
+                        Console.WriteLine($"{operand1 - operand2}");
+                        break;
+                    case "*":
+                        Console.WriteLine($"{operand1 * operand2}");
+                        break;
+                    case "/":
+                        Console.WriteLine($"{operand1 / operand2}");
+                        break;
+                    default:
+                        Console.WriteLine("Такого арифметического знака нет");
+                        break;
+                }
+            }
+
+            Console.ReadKey();
         }
     }
 }
