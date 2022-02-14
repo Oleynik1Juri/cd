2ed832db95a85a092981877e56486b85c1868606
OleynikJuri <Oleynikjuri@outlook.com>
2022-02-14 17:46:13 +0200

    CyclicDesignsApp

--- projects/CyclicDesignsApp/CyclicDesignsApp/Program.cs
+++ projects/CyclicDesignsApp/CyclicDesignsApp/Program.cs
@@ -10,6 +10,26 @@ namespace CyclicDesignsApp
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите число a: ");
+            var a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите число b: ");
+            var b = Convert.ToInt32(Console.ReadLine());
+
+            var result = 0;
+
+            for (var x = a; a < b; a++)
+            {
+                if ((a % 2) != 0)
+                {
+                    Console.WriteLine("{0} ", a);
+                }
+
+                result += a;
+            }
+
+            Console.WriteLine("Сума чисел {0}", result);
+            Console.ReadKey();
         }
     }
 }
