76b1264c73c68530d5c4e6d98e3f3f0f9da307d2
OleynikJuri <Oleynikjuri@outlook.com>
2022-02-14 18:09:50 +0200

    DeliveryRouteCalculationApp

--- projects/DeliveryRouteCalculationApp/DeliveryRouteCalculationApp/Program.cs
+++ projects/DeliveryRouteCalculationApp/DeliveryRouteCalculationApp/Program.cs
@@ -10,6 +10,19 @@ namespace DeliveryRouteCalculationApp
     {
         static void Main(string[] args)
         {
+            Console.Write("Число клиентов: ");
+            var customer = Convert.ToInt32(Console.ReadLine());
+
+            var result = 1;
+
+            do
+            {
+                result *= customer--;
+
+            } while (customer > 0);
+
+            Console.Write("Варианты доставки: {0}", result);
+            Console.ReadKey();
         }
     }
 }
